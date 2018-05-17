b5b8fac947ea1e53d05d2e18acbb276e70b19659
Scott Batey <sebateyjr@yahoo.com>
2018-05-17 11:58:49 -0400

    Added score and audio

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,16 @@
+import java.net.URL;
 import java.util.Random;
-import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Shape;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,6 +19,11 @@ public class Game extends Application{
 	// GAME OPTIONS
 	private static final int ROWSXCOLS = 4;
 	private static final int SIZE = ROWSXCOLS * ROWSXCOLS;
+	private static final boolean RANDOMGENERATOR = true;
+	private static final String GAMEOVER = "GAME OVER";
+	private static final int OFFSET = 37;
+	
+	// Sizing Options
 	private static final int WIDTH = 300;
 	private static final int HEIGHT = 400;
 	private static final int CANVAS_WIDTH = 250;
@@ -30,8 +31,8 @@ public class Game extends Application{
 	private static final int TILE_WIDTH = 50;
 	private static final int TILE_HEIGHT = 50;
 	private static final int TILE_PADDING = 10;
-	private static final boolean RANDOMGENERATOR = true;
-	private static final String GAMEOVER = "GAME OVER";
+	
+	// Color Options
 	private static final Color[] c = 
 			new Color[] {Color.web("#FFFFCC"),
 						 Color.web("#FFFF66"),
@@ -47,14 +48,24 @@ public class Game extends Application{
 						 Color.web("#33B3B3"),
 						 Color.web("#33FF66")};
 	
+	// Sound Options
+	private static final String scoreSoundPath = "audio/score.wav";
+	private static final String gameoverSoundPath = "audio/gameover.wav";
+	
 	// Game Variables
 	private Tile[][] board;
 	private Random random;
 	private boolean isMoving;
+	private int scoreValue;
+	private URL scoreSound,
+				gameoverSound;
+	private AudioClip scoreClip,
+					  gameoverClip;
 	
 	// Scene Variables
 	private Canvas canvas;
 	private GraphicsContext gc;
+	private Text score;
 	private Button upButton,
 		   		   downButton,
 		   		   leftButton,
@@ -83,16 +94,16 @@ public class Game extends Application{
 		stage.setTitle("2048");
 		
 		upButton = new Button("^");
-		upButton.setOnAction(e -> {moveUP();});
+		upButton.setOnAction(e -> { moveUP(); });
 		
 		downButton = new Button("v");
-		downButton.setOnAction(e -> {moveDOWN();});
+		downButton.setOnAction(e -> { moveDOWN(); });
 		
 		leftButton = new Button("<");
-		leftButton.setOnAction(e -> {moveLEFT();});
+		leftButton.setOnAction(e -> { moveLEFT(); });
 		
 		rightButton = new Button(">");
-		rightButton.setOnAction(e -> {moveRIGHT();});
+		rightButton.setOnAction(e -> { moveRIGHT(); });
 		
 		upButton.setMinSize(30, 30);
 		downButton.setMinSize(30, 30);
@@ -103,6 +114,8 @@ public class Game extends Application{
 		gc = canvas.getGraphicsContext2D();
 		paintBoard();
 		
+		score = new Text("Score: " + String.valueOf(scoreValue));
+		
 		HBox horizontalButtonHolder = new HBox();
 		VBox verticalButtonHolder = new VBox();
 		
@@ -111,7 +124,7 @@ public class Game extends Application{
 		horizontalButtonHolder.setAlignment(Pos.CENTER);
 		
 		VBox root = new VBox();
-		root.getChildren().addAll(canvas, horizontalButtonHolder);
+		root.getChildren().addAll(score, canvas, horizontalButtonHolder);
 		root.setAlignment(Pos.CENTER);
 		
 		Scene scene = new Scene(root, WIDTH, HEIGHT);
@@ -144,6 +157,13 @@ public class Game extends Application{
 	public void gameInit() {
 		board = new Tile[ROWSXCOLS][ROWSXCOLS];
 		random = new Random();
+		scoreValue = 0;
+		
+		scoreSound = getClass().getResource(scoreSoundPath);
+		scoreClip = new AudioClip(scoreSound.toString());
+		
+		gameoverSound = getClass().getResource(gameoverSoundPath);
+		gameoverClip = new AudioClip(gameoverSound.toString());
 		
 		for(int i = 0; i < ROWSXCOLS; i++) {
 			for(int j = 0; j < ROWSXCOLS; j++) {
@@ -164,10 +184,12 @@ public class Game extends Application{
 	 */
 	public void checkIfGameOver() {
 		if(initializeNewTile() || canMove()) {
+			score.setText("Score: " + String.valueOf(scoreValue));
 			paintBoard();
 		} else {
 			gc.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-			gc.strokeText(GAMEOVER, CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+			gc.strokeText(GAMEOVER, (CANVAS_WIDTH / 2) - OFFSET, CANVAS_HEIGHT / 2);
+			gameoverClip.play();
 		}
 	}
 	
@@ -175,11 +197,15 @@ public class Game extends Application{
 		gc.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		for(int i = 0; i < ROWSXCOLS; i++) {
 			for(int j = 0; j < ROWSXCOLS; j++) {
+				
+				//Get the x and y location to paint the tile
 				double x = board[i][j].getLocX() * (TILE_WIDTH + TILE_PADDING) + TILE_PADDING;
 				double y = board[i][j].getLocY() * (TILE_HEIGHT + TILE_PADDING) + TILE_PADDING;
 				gc.setFill(c[(int)(Math.log(board[i][j].getValue()) / Math.log(2))]);
 				gc.fillRect(x, y, TILE_WIDTH, TILE_HEIGHT);
 				gc.setStroke(Color.BLACK);
+				
+				//Draw Tile Value if greater than 0
 				if(board[i][j].getValue() > 0) {
 					gc.strokeText(String.valueOf(board[i][j].getValue()), 
 							x + (TILE_WIDTH / 2), y + (TILE_WIDTH / 2));
@@ -200,6 +226,7 @@ public class Game extends Application{
 					if(board[i][j].getValue() > 0 && //Don't check empty Tiles
 					   board[i][j].getValue() == board[i][j - 1].getValue()) {
 						board[i][j - 1].merge(board[i][j]);
+						incrementScore(board[i][j - 1].getValue());
 						isMoving = true;
 					} 
 					
@@ -232,6 +259,7 @@ public class Game extends Application{
 					if(board[i][j].getValue() > 0 && //Don't check empty Tiles
 					   board[i][j].getValue() == board[i][j + 1].getValue()) {
 						board[i][j + 1].merge(board[i][j]);
+						incrementScore(board[i][j + 1].getValue());
 						isMoving = true;
 					} 
 					
@@ -265,6 +293,7 @@ public class Game extends Application{
 					if(board[i][j].getValue() > 0 && //Don't check empty Tiles
 					   board[i][j].getValue() == board[i - 1][j].getValue()) {
 						board[i - 1][j].merge(board[i][j]);
+						incrementScore(board[i - 1][j].getValue());
 						isMoving = true;
 					} 
 					
@@ -297,6 +326,7 @@ public class Game extends Application{
 					if(board[i][j].getValue() > 0 && //Don't check empty Tiles
 					   board[i][j].getValue() == board[i + 1][j].getValue()) {
 						board[i + 1][j].merge(board[i][j]);
+						incrementScore(board[i + 1][j].getValue());
 						isMoving = true;
 					} 
 					
@@ -315,6 +345,11 @@ public class Game extends Application{
 		//due to new Tiles becoming empty.
 		while (isMoving);
 		checkIfGameOver();
+	}
+	
+	public void incrementScore(int value) {
+		scoreClip.play();
+		scoreValue += value;
 	}
 	
 	/* Checks to see if there is at least one move where 
